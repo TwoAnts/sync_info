8cc7ac49561135308669609363e3a873cff7cfc6
TwoAnts <lzmyhzy@gmail.com>
2017-03-20 19:56:43 +0800

    Add log to sync.py.

--- sync.py
+++ sync.py
@@ -6,6 +6,24 @@ from get_info import get_current_ip, get_hostname
 import json
 import urllib2
 import sys
+import logging
+from logging.handlers import RotatingFileHandler
+import traceback
+import os
+
+LOG_FILENAME = 'sync.log'
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+LOG_FILEPATH = os.path.join(CUR_DIR, LOG_FILENAME)
+
+logger = logging.getLogger('sync')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+                '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+handler = RotatingFileHandler(LOG_FILEPATH, maxBytes=1024)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.addHandler(logging.StreamHandler())
 
 
 def sync(url):
@@ -15,8 +33,12 @@ def sync(url):
     data = json.dumps(data).encode('utf-8')
     
     req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
-    resp = urllib2.urlopen(req)
-    print '%s %s %s' %(url, resp.getcode(), resp.read())
+    try:
+        resp = urllib2.urlopen(req)
+    except:
+        logger.info(traceback.format_exc())
+        return False
+    logger.info('%s %s %s' %(url, resp.getcode(), resp.read()))
     if resp.getcode() == 200:
         return True
     return False
