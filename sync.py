7e82171ea04769f317dde8c9a3ecca3dabeb36ed
TwoAnts <lzmyhzy@gmail.com>
2017-03-20 20:23:20 +0800

    Try more times when sync.

--- sync.py
+++ sync.py
@@ -5,11 +5,9 @@ from get_info import get_current_ip, get_hostname
 
 import json
 import urllib2
-import sys
-import logging
+import sys, os, time
+import logging, traceback
 from logging.handlers import RotatingFileHandler
-import traceback
-import os
 
 LOG_FILENAME = 'sync.log'
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -26,30 +24,30 @@ logger.addHandler(handler)
 logger.addHandler(logging.StreamHandler())
 
 
-def sync(url):
+def sync(url, try_times=3, try_delay=60):
     data = {}
     data['ip'] = get_current_ip()
     data['hostname'] = get_hostname()
     data = json.dumps(data).encode('utf-8')
     
     req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
-    try:
-        resp = urllib2.urlopen(req)
-    except:
-        logger.info(traceback.format_exc())
-        return False
-    logger.info('%s %s %s' %(url, resp.getcode(), resp.read()))
-    if resp.getcode() == 200:
-        return True
+    times = 0
+    except_msg = None
+    while times < try_times:
+        try:
+            resp = urllib2.urlopen(req)
+            logger.info('%s %s %s' %(url, resp.getcode(), resp.read()))
+            if resp.getcode() == 200: return True
+        except:
+            except_msg = traceback.format_exc()
+            times += 1 
+        time.sleep(try_delay)
+    if times >= try_times: logger.error(
+                    'try %s time, but failed.\n%s' %(times, except_msg))
     return False
-    
-
-    
-    
+        
 
 if __name__ == '__main__':
     URL = "http://%s/sync" %sys.argv[1]
     sync(URL)
     
-
-
